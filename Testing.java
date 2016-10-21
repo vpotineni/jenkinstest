261368121a2c0b459b3d2ad42c997b8ecbe07b2d
vpotineni@gmail.com <vpotineni@gmail.com>
2016-10-21 15:25:04 -0400

    initial version for Testing

---
+++ Testing.java
@@ -0,0 +1,13 @@
+package com.fannie;
+
+public class Testing{
+
+	public static int add(int a, int b){
+	return a+b;
+}
+	public static void main(String args[])
+{
+		System.out.println("Addition is" + add(10,30));
+}
+}
+
